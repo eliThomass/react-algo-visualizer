ef68eeb9871bf6368350e58e405ea81c6ddbf8d4
Eli Thomas <38369558+eliThomass@users.noreply.github.com>
2025-11-23 22:49:14 -0800

    added DFS and BFS visualization side-by-side

--- src/App.jsx
+++ src/App.jsx
@@ -3,70 +3,142 @@ import './App.css'
 import { dfs, bfs } from './algorithms'
 
 function App() {
+
+  const [isMouseDown, setIsMouseDown] = useState(false);
+  const [drawValue, setDrawValue] = useState(1);
+
   const [maze, setMaze] = useState(() => {
-    return Array(10).fill().map(() => Array(10).fill(0));
+    return Array(15).fill().map(() => Array(15).fill(0));
+  })
+  const [maze2, setMaze2] = useState(() => {
+    return Array(15).fill().map(() => Array(15).fill(0));
   })
 
-  const handleCell = (row, col, newValue) => {
-    setMaze((prevMaze) => {
-      return prevMaze.map((gridRow, rIndex) => {
-        if (rIndex === row) {
-          return gridRow.map((cell, cIndex) => {
-            return cIndex === col ? newValue : cell;
-          });
-        }
-        return gridRow;
+  const handleCell = (type, row, col, newValue) => {
+    if(type == 'DFS') {
+      setMaze((prevMaze) => {
+        return prevMaze.map((gridRow, rIndex) => {
+          if (rIndex === row) {
+            return gridRow.map((cell, cIndex) => {
+              return cIndex === col ? newValue : cell;
+            });
+          }
+          return gridRow;
+        });
       });
-    });
+    }
+    if(type == 'BFS') {
+      setMaze2((prevMaze) => {
+        return prevMaze.map((gridRow, rIndex) => {
+          if (rIndex === row) {
+            return gridRow.map((cell, cIndex) => {
+              return cIndex === col ? newValue : cell;
+            });
+          }
+          return gridRow;
+        });
+      });
+    }
+
   };
 
-  const visualizePathDFS = () => {
-    let mazeCopy = JSON.parse(JSON.stringify(maze));
-    let history = [];
+  const handleMouseDown = (row, col, currentVal) => {
+    setIsMouseDown(true);
     
-    dfs(0, 0, [2, 2], mazeCopy, new Set(), history);
-
-    history.forEach((step, index) => {
-      setTimeout(() => {
-        handleCell(step.r, step.c, step.val);
-      }, 100 * index);
-    });
+    const newVal = currentVal === 1 ? 0 : 1; 
+    setDrawValue(newVal);
+    handleCell(row, col, newVal);
+    handleCell('DFS', row, col, drawValue);
+    handleCell('BFS', row, col, drawValue);
   };
 
-  const visualizePathBFS = () => {
-    let mazeCopy = JSON.parse(JSON.stringify(maze));
-    let history = [];
-    
-    bfs(0, 0, [2, 2], mazeCopy, new Set(), history);
+  const handleMouseEnter = (row, col) => {
+    if (isMouseDown) {
+      handleCell('DFS', row, col, drawValue);
+      handleCell('BFS', row, col, drawValue);
+    }
+  };
 
-    history.forEach((step, index) => {
+  const handleMouseUp = () => {
+    setIsMouseDown(false);
+  };
+
+  const visualizePath = () => {
+    let mazeCopy = JSON.parse(JSON.stringify(maze));
+    let historyDFS = [];
+    let mazeCopy2 = JSON.parse(JSON.stringify(maze));
+    let historyBFS = [];
+    
+    bfs(0, 0, [10, 10], mazeCopy, new Set(), historyBFS);
+    dfs(0, 0, [10, 10], mazeCopy2, new Set(), historyDFS);
+
+    const maxSteps = Math.max(historyDFS.length, historyBFS.length);
+
+    for (let i = 0; i < maxSteps; i++) {
       setTimeout(() => {
-        handleCell(step.r, step.c, step.val);
-      }, 100 * index);
-    });
+        if (i < historyDFS.length) {
+          const stepA = historyDFS[i];
+          handleCell('DFS', stepA.r, stepA.c, stepA.val);
+        }
+
+        if (i < historyBFS.length) {
+          const stepB = historyBFS[i];
+          handleCell('BFS', stepB.r, stepB.c, stepB.val);
+        }
+
+      }, 40 * i);
+    }
   };
 
   return (
     <>
-      <div id='container'>
-        <button onClick={() => visualizePathDFS()}> DFS Visualization </button>
-        <button onClick={() => visualizePathBFS()}> BFS Visualization </button>
-        <p className='grid'>{maze.map((row, rowIndex) => (
-          <div key={rowIndex}>
+      <div id='container' onMouseUp={handleMouseUp}>
+        <button onClick={() => visualizePath()}> DFS Visualization </button>
+
+        <div className='grids'>
+          <p className='grid' onMouseLeave={handleMouseUp}>{maze.map((row, rowIndex) => (
+            <div key={rowIndex} className='row'>
+              {row.map((item, itemIndex) => (
+                <button key={itemIndex} 
+                  onMouseDown={() => handleMouseDown(rowIndex, itemIndex, item)}
+                  onMouseEnter={() => handleMouseEnter(rowIndex, itemIndex)}
+                  className='cell' style={{
+                    backgroundColor: 
+                      item === 1 ? "black" 
+                      : item === 2 ? "rgba(247, 244, 86, 1)" 
+                      : item === 3 ? "rgba(97, 178, 70, 1)" 
+                      : item === 4 ? "red" 
+                      : "rgb(233, 233, 233)"
+                }}>
+                  
+                </button>
+              ))}
+            </div>
+         ))}
+        </p>
+
+        <p className='grid' onMouseLeave={handleMouseUp}>{maze2.map((row, rowIndex) => (
+          <div key={rowIndex} className='row'>
             {row.map((item, itemIndex) => (
-              <button key={itemIndex} onClick={() => handleCell(rowIndex, itemIndex, 1)} className='cell' style={{
-                backgroundColor: 
-                  item === 1 ? "black" 
-                  : item === 2 ? "rgba(247, 244, 86, 1)" 
-                  : item === 3 ? "rgba(97, 178, 70, 1)" 
-                  : item === 4 ? "red" 
-                  : "rgb(233, 233, 233)"
+              <button key={itemIndex} 
+                onMouseDown={() => handleMouseDown(rowIndex, itemIndex, item)}
+                onMouseEnter={() => handleMouseEnter(rowIndex, itemIndex)}
+                className='cell' style={{
+                  backgroundColor: 
+                    item === 1 ? "black" 
+                    : item === 2 ? "rgba(247, 244, 86, 1)" 
+                    : item === 3 ? "rgba(97, 178, 70, 1)" 
+                    : item === 4 ? "red" 
+                    : "rgb(233, 233, 233)"
               }}>
                 
               </button>
             ))}
           </div>
-        ))}</p>
+          ))}
+        </p>
+        </div>
+        
       </div>
     </>
   )
